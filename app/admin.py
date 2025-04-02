c00ea3840ba9001c05fe4f8c660680328f3859f5
Asror <asrorrustmaovich007@gmail.com>
2025-04-02 15:55:22 +0500

    asdaS

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Role, User, product_type, Product, OrderProduct, Status, Order
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 # Role modelini admin panelga qo'shish
 @admin.register(Role)
 class RoleAdmin(admin.ModelAdmin):
@@ -10,11 +10,17 @@ class RoleAdmin(admin.ModelAdmin):
 
 # User modelini admin panelga qo'shish
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
     list_display = ('id', 'username', 'email', 'phone', 'role', 'is_available')
     list_filter = ('role', 'is_available')
     search_fields = ('username', 'email', 'phone')
     ordering = ('-id',)
+
+    # Parolni avtomatik hash qilish
+    def save_model(self, request, obj, form, change):
+        if change and 'password' in form.changed_data:
+            obj.set_password(obj.password)
+        super().save_model(request, obj, form, change)
 
 # Product Type modelini admin panelga qo'shish
 @admin.register(product_type)
